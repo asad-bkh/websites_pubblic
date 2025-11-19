cab5622eefe3484b6f1f285c8c28c1d109f094ec
asad-bkh <asadbkh@gmail.com>
2025-11-19 14:44:24 +0100

    Update site-footer-signature-it.js

--- site-footer-signature-it.js
+++ site-footer-signature-it.js
@@ -1,5 +1,9 @@
-document.write(
-  '<div style="text-align:center; font-size:14px; padding:6px 0;">' +
-    'Sito realizzato da <a href="https://digital1.it" target="_blank" style="color:inherit; text-decoration:underline;">Digital1</a>' +
-  '</div>'
-);
+document.addEventListener("DOMContentLoaded", function() {
+  var footer = document.querySelector("footer") || document.body;
+  var div = document.createElement("div");
+  div.style.textAlign = "center";
+  div.style.fontSize = "14px";
+  div.style.padding = "6px 0";
+  div.innerHTML = 'Sito realizzato da <a href="https://digital1.it" target="_blank" style="color:inherit; text-decoration:underline;">Digital1</a>';
+  footer.appendChild(div);
+});
