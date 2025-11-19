67ec00bd0c2fd3fe68daa8e777f3d12497641019
asad-bkh <asadbkh@gmail.com>
2025-11-19 14:22:47 +0100

    Add files via upload

---
+++ site-footer-signature-it.js
@@ -0,0 +1,5 @@
+document.write(
+  '<div style="text-align:center; font-size:14px; padding:6px 0;">' +
+    'Sito realizzato da <a href="https://digital1.it" target="_blank" style="color:inherit; text-decoration:underline;">Digital1</a>' +
+  '</div>'
+);
